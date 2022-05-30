62bc2127b6e613900c29e7a0b8828bd782d2a7e3
darkavengersmr <mailsea@yandex.ru>
2022-05-30 19:42:18 +0400

    update tests for investment

--- api_tests.py
+++ api_tests.py
@@ -18,7 +18,7 @@ class LocalStorage:
         self.email = None
         self.test_investment_item_id = None
 
-    async def test_get_token(self) -> None:
+    async def get_token(self) -> None:
         if not self.token:
             data = {'username': self.username, 'password': self.password}
             response = await requests.post(f'{self.socket}/token', data=data)
@@ -30,19 +30,17 @@ class LocalStorage:
             mydata = ast.literal_eval(response.content.decode("UTF-8"))
             self.user_id = mydata['id']
             self.email = mydata['email']
-            return mydata['username']
-        return self.username
+
 
 storage = LocalStorage()
 
 
 class TestApi(aiounittest.AsyncTestCase):
-    async def test_get_user_info(self) -> None:
-        username = await storage.test_get_token()
-        self.assertTrue(username == storage.username)
+    async def test_create_and_delete_investment(self) -> None:
+        # refresh token, get user id
+        await storage.get_token()
 
-    async def test_create_and_delete_invesment(self) -> None:
-        await storage.test_get_token()
+        # test create new investment
         headers = {"Authorization": f"Bearer {storage.token}"}
         params = {"user_id": storage.user_id}
         json = {"description": "test_item", "category_id": None}
@@ -52,6 +50,25 @@ class TestApi(aiounittest.AsyncTestCase):
         storage.test_investment_item_id = mydata['id']
         self.assertTrue(mydata['description'] == "test_item")
 
+        # test update investment
+        headers = {"Authorization": f"Bearer {storage.token}"}
+        params = {"user_id": storage.user_id}
+        json = {"id": storage.test_investment_item_id, "description": "update_test_item", "category_id": None}
+        response = await requests.put(f'{storage.socket}/users/investment_items/',
+                                      headers=headers, params=params, json=json)
+        mydata = ast.literal_eval(response.content.decode("UTF-8"))
+        self.assertTrue(mydata['result'] == "investment updated")
+
+        # test read investment
+        headers = {"Authorization": f"Bearer {storage.token}"}
+        params = {"user_id": storage.user_id}
+        response = await requests.get(f'{storage.socket}/users/investment_items/',
+                                      headers=headers, params=params)
+        mydata = ast.literal_eval(response.content.decode("UTF-8").replace("null", "None"))
+        new_investment = [d for d in mydata["investments"] if d['id'] == storage.test_investment_item_id][0]
+        self.assertTrue(new_investment['description'] == "update_test_item")
+
+        # test delete investment
         params = {"user_id": storage.user_id, "investment_id": storage.test_investment_item_id}
         response = await requests.delete(f'{storage.socket}/users/investment_items/',
                                          headers=headers, params=params)
